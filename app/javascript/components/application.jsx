0a75d9fe4646a18a0d1271e3113aba7c49fc3a4a
Slava Eremenko <slava.eremenko@gmail.com>
2022-12-16 18:46:32 -0600

    Install NextUI & first button

--- app/javascript/components/application.jsx
+++ app/javascript/components/application.jsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { NextUIProvider } from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
+
 
 class App extends React.Component {
 	render() {
-		return <h1>Hello, World!</h1>;
+		return (
+			<div>
+				<h1>Hello, World!</h1>
+				<Button>Click</Button>
+			</div>
+		);
 	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.createRoot(document.getElementById("root")).render(
-  	<App />
+	<NextUIProvider>
+  		<App />
+	</NextUIProvider>
   );
 });
